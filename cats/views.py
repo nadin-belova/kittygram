0ca337a1f7c4d40cddd5a9bed40ad4ee67f5fc1b
Надежда Белова <belovanadezhda1983@gmail.com>
2023-04-07 17:53:32 +0300

    init commit

--- cats/views.py
+++ cats/views.py
@@ -9,11 +9,35 @@ from .serializers import CatSerializer
 @api_view(['GET', 'POST'])
 def cat_list(request):
     if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
+        serializer = CatSerializer(data=request.data, many=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def one_cat(request, pk):
+    try:
+        cat = Cat.objects.get(id=pk)
+    except Cat.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = CatSerializer(cat)
+        return Response(serializer.data)
+
+    elif request.method in ('PUT', 'PATCH'):
+        serializer = CatSerializer(cat, data=request.data, partial=True) 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path
 
-from cats.views import cat_list
+from cats.views import cat_list, one_cat
+
 
 urlpatterns = [
    path('cats/', cat_list),
+   path('cats/<int:pk>/', one_cat)
 ]
-
-
